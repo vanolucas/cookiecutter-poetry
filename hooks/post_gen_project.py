3eec7096dc6142c9ecebf748513ba4110ce2ff82
vanolucas <10572472+vanolucas@users.noreply.github.com>
2024-05-25 10:17:57 +0200

    feat: remove CI/CD, add PostgreSQL + Adminer options (#1)
    * feat: removed CI/CD, added PostgreSQL and Adminer options
    
    * fix(LICENSE): typo in name
    
    * chore: documented new cookiecutter options

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,7 +6,6 @@ project_name = "{{ cookiecutter.__project_name_snake_case }}"
 development_environment = "{{ cookiecutter.development_environment }}"
 with_fastapi_api = int("{{ cookiecutter.with_fastapi_api }}")
 with_typer_cli = int("{{ cookiecutter.with_typer_cli }}")
-continuous_integration = "{{ cookiecutter.continuous_integration }}"
 is_application = "{{ cookiecutter.project_type == 'app' }}" == "True"
 
 # Remove py.typed and Dependabot if not in strict mode.
@@ -23,16 +22,3 @@ if not with_fastapi_api:
 if not with_typer_cli:
     os.remove(f"src/{project_name}/cli.py")
     os.remove("tests/test_cli.py")
-
-# Remove the continuous integration provider that is not selected.
-if continuous_integration != "GitHub":
-    shutil.rmtree(".github/")
-elif continuous_integration != "GitLab":
-    os.remove(".gitlab-ci.yml")
-
-# Remove unused GitHub Actions workflows.
-if continuous_integration == "GitHub":
-    if is_application:
-        os.remove(".github/workflows/publish.yml")
-    else:
-        os.remove(".github/workflows/deploy.yml")
